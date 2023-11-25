14a16f9a9189df3899ba26391782a089d87b115c
hunteryavitz <hunteryavitz@protonmail.com>
2023-11-25 06:09:47 -0700

    refactor: clean comments

--- app.js
+++ app.js
@@ -1,8 +1,9 @@
+const cookieParser = require('cookie-parser');
 const createError = require('http-errors');
+const logger = require('morgan');
+
 const express = require('express');
 const path = require('path');
-const cookieParser = require('cookie-parser');
-const logger = require('morgan');
 
 const bodyParser=require('body-parser');
 const cors = require('cors');
@@ -12,7 +13,6 @@ const usersRouter = require('./routes/users');
 
 const app = express();
 
-// view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
@@ -22,39 +22,24 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use(cors());//cors
-app.use(bodyParser.json());//body-parser
-app.use(bodyParser.urlencoded({extended:false}));//body-parser
+app.use(cors());
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended:false}));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
-// catch 404 and forward to error handler
 app.use(function(req, res, next) {
   next(createError(404));
 });
 
-// error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
 
-  // render the error page
   res.status(err.status || 500);
   res.render('error');
 });
-
-// app.get("/",(req,res)=>{
-//   res.send("Hello World");
-// })
-//
-// app.post("/login",(req,res)=>{
-//   res.json(req.body)
-// })
-//
-// app.listen(3000,()=>{
-//   console.log("Server running on port 3000");
-// })
 
 module.exports = app;
